80df8d72ec3b33ef01800365741800be5e5c2183
EC2 Default User <ec2-user@ip-172-31-46-231.ap-south-1.compute.internal>
2026-07-17 10:26:16 +0000

    Added Docker Compose and updated Spring Boot application

---
+++ my-app/src/main/java/com/example/WelcomeController.java
@@ -0,0 +1,14 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class WelcomeController {
+
+    @GetMapping("/welcome/{name}")
+    public String welcome(@PathVariable String name) {
+        return "Welcome " + name + "!";
+    }
+}
